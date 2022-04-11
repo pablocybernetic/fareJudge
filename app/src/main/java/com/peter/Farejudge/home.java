f35f55d1c23fff24cf991484597c47be0a417bd0
[peter gitau] <gitaup08@gmail.com>
2022-04-11 13:34:55 +0300

    list view complete Sqlite remaining

--- app/src/main/java/com/peter/Farejudge/home.java
+++ app/src/main/java/com/peter/Farejudge/home.java
@@ -1,11 +1,16 @@
 package com.peter.Farejudge;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -16,11 +21,56 @@ import com.google.android.material.navigation.NavigationBarView;
 public class home extends AppCompatActivity {
 
     TextView name, mail;
-    Button logout,activity;
+    Button logout,activity,insert, view1;
+    EditText name1, contact, dob;
+    DBHelper DB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        name1 = findViewById(R.id.estname);
+        contact = findViewById(R.id.estlocation);
+        dob= findViewById(R.id.review);
+        insert = findViewById(R.id.btnInsert);
+        DB = new DBHelper(this);
+        view1 =findViewById(R.id.view1);
+
+        insert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nameTXT = name1.getText().toString();
+                String contactTXT = contact.getText().toString();
+                String dobTXT = dob.getText().toString();
+
+                Boolean checkinsertdata = DB.insertuserdata(nameTXT, contactTXT, dobTXT);
+                if(checkinsertdata==true)
+                    Toast.makeText(home.this, "New Entry Inserted", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(home.this, "New Entry Not Inserted", Toast.LENGTH_SHORT).show();
+            }        });
+
+        view1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cursor res = DB.getdata();
+                if(res.getCount()==0){
+                    Toast.makeText(home.this, "No Entry Exists", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                StringBuffer buffer = new StringBuffer();
+                while(res.moveToNext()){
+                    buffer.append("Name :"+res.getString(0)+"\n");
+                    buffer.append("Contact :"+res.getString(1)+"\n");
+                    buffer.append("Date of Birth :"+res.getString(2)+"\n\n");
+                }
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(home.this);
+                builder.setCancelable(true);
+                builder.setTitle("User Entries");
+                builder.setMessage(buffer.toString());
+                builder.show();
+            }        });
+
 
 //        logout = findViewById(R.id.logout);
         name = findViewById(R.id.name);
